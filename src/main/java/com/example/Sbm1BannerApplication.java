ffdf87f23eb1b60f36e8fad16d13162c0d856293
sarang <devops1sarang@gmail.com>
2020-12-05 17:43:31 +0530

    changes done in master

--- src/main/java/com/example/Sbm1BannerApplication.java
+++ src/main/java/com/example/Sbm1BannerApplication.java
@@ -1,16 +1,20 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Sbm1BannerApplication {
+	private Map<String, Object> map = new HashMap<String, Object>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sbm1BannerApplication.class, args);
 	}
 
-public void m1(){
-}
+	public void m1() {
+	}
 
 }
