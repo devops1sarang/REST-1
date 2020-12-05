afcd46c7bed85b615b1356ac4073478129ec92f0
sarang <devops1sarang@gmail.com>
2020-12-05 17:41:01 +0530

    changes done

--- src/main/java/com/example/Sbm1BannerApplication.java
+++ src/main/java/com/example/Sbm1BannerApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Sbm1BannerApplication {
+	private String name = "sarang";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sbm1BannerApplication.class, args);
 	}
 
-public void m1(){
-}
+	public void m1() {
+	}
 
 }
