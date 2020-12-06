bd793bc982f41e0a3baf9c444248e824a274f12c
sarang <devops1sarang@gmail.com>
2020-12-06 06:54:40 +0530

    done

--- src/main/java/com/example/Sbm1BannerApplication.java
+++ src/main/java/com/example/Sbm1BannerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sbm1BannerApplication {
 	private Map<String, Object> map = new HashMap<String, Object>();
-	private String name = "sarang";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sbm1BannerApplication.class, args);
